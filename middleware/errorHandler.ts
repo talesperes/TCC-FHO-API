b9127895e14cdcb26c0b41ef137b8a14458900e8
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2023-03-16 13:07:56 -0300

    Update errorHandler.ts

--- middleware/errorHandler.ts
+++ middleware/errorHandler.ts
@@ -1,28 +1,30 @@
-const errorHandler = (fn: any) => async (...args: any) => {
+const errorHandler =
+  (fn: any) =>
+  async (...args: any) => {
     try {
       const result = await fn(...args);
-  
+
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(result),
       };
     } catch (error: any) {
       const statusCode = error.statusCode || 500;
-      const message = error.message || 'Erro interno';
-  
+      const message = error.message || "Erro interno";
+
       return {
         statusCode,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify({ message }),
       };
     }
   };
-  
+
 export default errorHandler;
