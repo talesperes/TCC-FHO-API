a0527ea251ff6c1006b287ead29338fb9cd0c492
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2023-03-16 13:44:57 -0300

    fix

--- modules/user/handler.ts
+++ modules/user/handler.ts
@@ -1,13 +1,27 @@
 import UserRepository from "./repository";
-import CreateUserUseCase from "./usecase/createuserUseCase";
+import CreateUserUseCase from "./usecase/createUserUseCase";
 import errorHandler from "../../middleware/errorHandler";
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const repository = new UserRepository(MONGODB_URI);
 const createUserUseCase = new CreateUserUseCase(repository);
 
-export const createUser = errorHandler(async (event: any) => {
-  const data = JSON.parse(event.body);
-  return await createUserUseCase.execute(data);
-});
+const createUserHandler: APIGatewayProxyHandler = errorHandler(
+  async (event: any): Promise<APIGatewayProxyResult> => {
+    const data = JSON.parse(event.body);
+    const result = await createUserUseCase.execute(data);
+
+    return {
+      statusCode: 201,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(result),
+    };
+  }
+);
+
+export { createUserHandler as createUser };
