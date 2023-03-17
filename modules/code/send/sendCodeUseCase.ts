09e59543dbde4bf48bc163141d1153dd1fc2ca42
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2023-03-17 13:25:01 -0300

    Update sendCodeUseCase.ts

--- modules/code/send/sendCodeUseCase.ts
+++ modules/code/send/sendCodeUseCase.ts
@@ -10,28 +10,13 @@ class SendCodeUseCase {
 
   async execute(username: string) {
     const code = Math.floor(Math.random() * 900000 + 100000).toString();
-    const message = `[Agendamento de Consulta] Seu código de verificação é: ${code}`;
     const user = await this.repository.getUser(username);
     const { UserAttributes } = user;
     if (UserAttributes) {
       const phoneNumberData = UserAttributes.find(
         (attr) => attr.Name === "phone_number"
       );
-      const lastTimeCodeData = UserAttributes.find(
-        (attr) => attr.Name === "custom:last_time_code"
-      );
       if (phoneNumberData?.Value) {
-        if (lastTimeCodeData?.Value) {
-          const savedTimestamp = lastTimeCodeData.Value;
-          const currentTimestamp = Date.now();
-          const diffGreaterThan60Seconds = this.diffGreaterThan60Seconds(
-            currentTimestamp,
-            savedTimestamp
-          );
-          if (!diffGreaterThan60Seconds) {
-            return;
-          }
-        }
         const phoneNumber = phoneNumberData.Value;
 
         const client = twilio(accountSid, authToken); 
@@ -39,16 +24,13 @@ class SendCodeUseCase {
         await client.verify.v2
           .services(serviceSid)
           .verifications.create({
-            to: "+5519992619918",
+            to: phoneNumber,
             channel: "sms",
           })
           .then((verification) => console.log(verification))
           .catch((error) => console.error(error));
 
         return { message: "code sended" };
-
-        // await this.repository.sendSMS(phoneNumber, message);
-        // await this.repository.saveCodeToCognito(username, code);
       } else {
         throw new Error("User doesnt have phone number");
       }
@@ -56,11 +38,6 @@ class SendCodeUseCase {
       throw new Error("User not found");
     }
   }
-
-  private diffGreaterThan60Seconds = (timestamp1: any, timestamp2: any) => {
-    const differenceInSeconds = Math.abs(timestamp1 - timestamp2) / 1000;
-    return differenceInSeconds > 60;
-  };
 }
 
 export { SendCodeUseCase };
