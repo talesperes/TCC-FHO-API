c1c6c94c9269b60fb000f5b6c9400143c0e45c04
Tales Oliveira <tales.oliveira@alunos.fho.edu.br>
2023-03-18 00:55:09 -0300

    Update SendCodeUseCase.ts

--- src/usecases/SendCodeUseCase.ts
+++ src/usecases/SendCodeUseCase.ts
@@ -14,36 +14,41 @@ class SendCodeUseCase {
 	}
 
 	async execute(cpf: string): Promise<IResponse> {
-		const user = await this.userRepository.getUserByCPF(cpf)
+		try {
+			const user = await this.userRepository.getUserByCPF(cpf)
 
-		if (!user) {
-			throw new UserNotFoundException()
+			if (!user) {
+				throw new UserNotFoundException()
+			}
+
+			const { phoneNumber, lastCodeTime } = user
+
+			const currentTime = Date.now()
+
+			const diffGreaterThanLimit = Math.abs(currentTime - lastCodeTime) / 1000 > 60
+
+			if (lastCodeTime && !diffGreaterThanLimit) {
+				throw new CodeAlreadySentException()
+			}
+
+			const sendCodeResponse = await this.codeService.send(phoneNumber)
+			const {
+				message,
+				data: { sid },
+			} = sendCodeResponse
+
+			const newLastCodeTime = Date.now()
+
+			await this.userRepository.updateUser(cpf, {
+				sid,
+				lastCodeTime: newLastCodeTime,
+			})
+
+			return { message }
+		} catch (error) {
+			console.log("DEU ERRO XD ", error)
+			throw new Error("")
 		}
-
-		const { phoneNumber, lastCodeTime } = user
-
-		const currentTime = Date.now()
-
-		const diffGreaterThanLimit = Math.abs(currentTime - lastCodeTime) / 1000 > 60
-
-		if (lastCodeTime && !diffGreaterThanLimit) {
-			throw new CodeAlreadySentException()
-		}
-
-		const sendCodeResponse = await this.codeService.send(phoneNumber)
-		const {
-			message,
-			data: { sid },
-		} = sendCodeResponse
-
-		const newLastCodeTime = Date.now()
-
-		await this.userRepository.updateUser(cpf, {
-			sid,
-			lastCodeTime: newLastCodeTime,
-		})
-
-		return { message }
 	}
 }
 
